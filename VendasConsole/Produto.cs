1423813f0b673ad3b106b814623f624ab87d6854
grleandro <gr.leandro.goncalves@gmail.com>
2020-08-17 20:54:04 -0300

    Cadastrar / Listar - Vendedor e Produto

---
+++ VendasConsole/Produto.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace VendasConsole
+{
+    class Produto
+    {
+        //Construtor
+        public Produto(string nome, double preco, int quantidade)
+        {
+            Nome = nome;
+            Preco = preco;
+            Quantidade = quantidade;
+        }
+        public Produto()
+        {
+            CriadoEm = DateTime.Now;
+        }
+        //Atributos, propriedades e característica
+        public string Nome { get; set; }
+
+        public double Preco { get; set; }
+
+        public int Quantidade { get; set; }
+
+        public DateTime CriadoEm { get; set; }
+
+        public override string ToString()
+        {
+            return $"Nome: {Nome} | Preço: {Preco} | Quantidade: {Quantidade} | Criado em: {CriadoEm}\n\n";
+        }
+    }
+}
---
+++ VendasConsole/Vendedor.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace VendasConsole
+{
+    class Vendedor
+    {
+        //Construtor
+        public Vendedor(string nome, string cpf)
+        {
+            Nome = nome;
+            Cpf = cpf;
+        }
+        public Vendedor()
+        {
+            CriadoEm = DateTime.Now;
+        }
+        //Atributos, propriedades e característica
+        public string Nome { get; set; }
+
+        public string Cpf { get; set; }
+
+        public DateTime CriadoEm { get; set; }
+
+        public override string ToString()
+        {
+            return $"Nome: {Nome} | CPF: {Cpf} | Criado em: {CriadoEm}\n\n";
+        }
+    }
+}
